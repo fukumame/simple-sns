70025901242671ca4c4ee033dcb6d227ca516597
Atsushi Fukuda <atsushi.fukuda@bm-sms.jp>
2017-05-04 22:48:35 +0900

    Move a method to retrieve user data to before_action callback to avoid duplicates.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
+  before_action :retrieve_user, only: [:follow, :unfollow]
 
   def index
     @users = current_user.others
   end
 
-  # def show
-  #
-  # end
-
   def follow
-    @user = User.find(params[:id])
     follow = current_user.follows.build(followed_user: @user)
     follow.save!
     render 'users/toggle_follow_button'
   end
 
   def unfollow
-    @user = User.find(params[:id])
     follow = current_user.follows.find_by(followed_user: @user)
     follow.destroy
     render 'users/toggle_follow_button'
+  end
+
+  private
+
+  def retrieve_user
+    @user = User.find(params[:id])
   end
 
 end
